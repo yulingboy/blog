3915f115aab7d2cf5fe73716f4b5e5cc1132ab50
‘yulingboy <yulinglsy@163.com>
2021-06-06 22:50:53 +0800

    修改token验证

--- blog-admin/src/axios.js
+++ blog-admin/src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // import { getUserCookie } from '@/utils/userCookie';
 import { Message } from 'element-ui';
+import router from '@/router'
 const instance = axios.create({
   // baseURL: 'http://127.0.0.1:3000/api/admin/v2/' 
   baseURL: 'http://127.0.0.1:3000/api/v1/blog'
@@ -18,9 +19,10 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   response => {
     if (response.data.meta.code !== 1000) {
-      // if (response.data.meta.code === 401) {
-      //   router.push('/');
-      // }
+      if (response.data.meta.code === 401) {
+        router.push('/login');
+        localStorage.clear()
+      }
       Message({
         showClose: true,
         message: response.data.meta.message,
